909423d2a047dc7baa36e16fb6d9df96437b7c96
andresrengifo2002 <raulrentellito@gmail.com>
2023-07-23 15:34:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Perfil.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bancoproyectos.MenuPrincipal;
+
+public class Perfil extends AppCompatActivity {
+
+
+    private TextView T1, T2, T3;
+
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil);
+        T2=findViewById(R.id.text2);
+        Intent intent=getIntent();
+        String accessT=intent.getStringExtra("token");
+        T2.setText(accessT);
+
+
+        T1 = (TextView) findViewById(R.id.text1);
+        T2 = (TextView) findViewById(R.id.text2);
+        T3 = (TextView) findViewById(R.id.text3);
+
+
+    }
+
+    public void onClick(View view) {
+
+
+            Intent intent = new Intent(Perfil.this, MenuPrincipal.class);
+            startActivity(intent);
+        }
+    }
